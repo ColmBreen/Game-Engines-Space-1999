e47b1dc2e697132bf84dbecd3b5c68541f4d0556
colm <colmbreen89@gmail.com>
2018-03-13 10:49:21 +0000

    lab test upload

---
+++ Space1999/Assets/Scripts/Boid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public float mass = 1;
+    public float maxSpeed = 10;
+    Vector3 leaderTarget = new Vector3(0, 0, 1000);
+    Vector3 velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Calculate();
+        
+	}
+
+    public Vector3 SeekForce(Vector3 target)
+    {
+        Vector3 desired = target - transform.position;
+        desired.Normalize();
+        desired *= maxSpeed;
+        return desired - velocity;
+    }
+
+    public Vector3 OffsetPursue()
+    {
+        float distance = Vector3.Distance(GameObject.Find("leader").transform.position , transform.position);
+        float time = distance / maxSpeed;
+        Vector3 leaderDistance = new Vector3(GameObject.Find("leader").transform.position.x, GameObject.Find("leader").transform.position.y,
+            GameObject.Find("leader").transform.position.z + (maxSpeed * time));
+        return SeekForce(leaderDistance);
+    }
+
+    public void Calculate()
+    {
+        if (this.name == "leader")
+        {
+            velocity += SeekForce(transform.position + leaderTarget);
+            transform.position += velocity;
+        }
+        else
+        {
+            Debug.Log("Offset Pursue");
+            velocity += OffsetPursue();
+        }
+    }
+}
---
+++ Space1999/Assets/Scripts/EagleSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+    public float gap = 20;
+    public float followers = 4;
+    public GameObject prefab;
+
+    private void Awake()
+    {
+        int j = 1;
+        bool left = true;
+        Vector3 leftGaps = new Vector3(gap, 0, gap);
+        Vector3 rightGaps = new Vector3(gap, 0, -gap);
+        GameObject leader = GameObject.Instantiate(prefab);
+        leader.name = "leader";
+        List<GameObject> followerShips = new List<GameObject>();
+        for(int i = 1; i <= (followers * 2); i++)
+        {
+            followerShips.Add(GameObject.Instantiate(prefab));
+            followerShips[i - 1].name = "follower";
+            if(left == true)
+            {
+                followerShips[i - 1].transform.position = leader.transform.position - (leftGaps * i);
+            }
+            else
+            {
+                followerShips[i - 1].transform.position = leader.transform.position + (rightGaps * j);
+                j++;
+            }
+
+            if(i == followers)
+            {
+                left = false;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
